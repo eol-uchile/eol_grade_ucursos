f0c19db7d8118d76aa944e7d17f490ab9741f3e9
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-05-09 12:26:32 -0400

    chore: updates test.sh to generate coverage badge

--- .github/test.sh
+++ .github/test.sh
@@ -1,13 +1,18 @@
 #!/bin/dash
-pip install -e git+https://github.com/eol-uchile/uchileedxlogin@1fdcdaa6c1b8719bd9a92914eaaf50e500bb35ff#egg=uchileedxlogin
-pip install -e /openedx/requirements/eol_grade_ucursos
 
-cd /openedx/requirements/eol_grade_ucursos/gradeucursos
-cp /openedx/edx-platform/setup.cfg .
+set -e
+
+pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/uchileedxlogin@0.0.1#egg=uchileedxlogin
+pip install --src /openedx/venv/src -e /openedx/requirements/app
+pip install pytest-cov genbadge[coverage]
+
+cd /openedx/requirements/app
+
 mkdir test_root
-cd test_root/
-ln -s /openedx/staticfiles .
+ln -s /openedx/staticfiles ./test_root/
 
-cd /openedx/requirements/eol_grade_ucursos/gradeucursos
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest gradeucursos/tests.py
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+rm -rf test_root
+
+genbadge coverage
